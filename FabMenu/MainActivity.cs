0c9e0bc6bcf7c878dde93284cb60aae41431b879
sdw <sdw@inea.com>
2018-08-02 14:12:09 -0400

    FabMenu tutorial

--- FabMenu/MainActivity.cs
+++ FabMenu/MainActivity.cs
@@ -2,18 +2,121 @@
 using Android.Widget;
 using Android.OS;
 using Android.Support.V7.App;
+using Android.Support.Design.Widget;
+using Android.Views;
+using System;
+using Android.Animation;
 
 namespace FabMenu
 {
-    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    [Activity(Label = "@string/app_name", MainLauncher = true)]
     public class MainActivity : AppCompatActivity
     {
+        private static bool isFabOpen;
+        private FloatingActionButton fabAirballoon;
+        private FloatingActionButton fabCake;
+        private FloatingActionButton fabMain;
+        private View bgFabMenu;
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
 
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.activity_main);
+
+            fabAirballoon = FindViewById<FloatingActionButton>(Resource.Id.fab_airballoon);
+            fabCake = FindViewById<FloatingActionButton>(Resource.Id.fab_cake);
+            fabMain = FindViewById<FloatingActionButton>(Resource.Id.fab_main);
+            bgFabMenu = FindViewById<View>(Resource.Id.bg_fab_menu);
+
+            fabMain.Click += (o, e) =>
+            {
+                if (!isFabOpen)
+                    ShowFabMenu();
+                else
+                    CloseFabMenu();
+            };
+
+            fabCake.Click += (o, e) =>
+            {
+                CloseFabMenu();
+                Toast.MakeText(this, "Cake!", ToastLength.Short).Show();
+            };
+
+            fabAirballoon.Click += (o, e) =>
+            {
+                CloseFabMenu();
+                Toast.MakeText(this, "Air Balloon!", ToastLength.Short).Show();
+            };
+
+            bgFabMenu.Click += (o, e) => CloseFabMenu();
+        }
+
+        private void CloseFabMenu()
+        {
+            isFabOpen = false;
+            fabAirballoon.Visibility = ViewStates.Visible;
+            fabCake.Visibility = ViewStates.Visible;
+            bgFabMenu.Visibility = ViewStates.Visible;
+
+            fabMain.Animate().Rotation(0f);
+            bgFabMenu.Animate().Alpha(0f);
+            fabAirballoon.Animate()
+                .TranslationY(0f)
+                .Rotation(90f);
+            fabCake.Animate()
+                .TranslationY(0f)
+                .Rotation(90f).SetListener(new FabAnimatorListener(bgFabMenu, fabCake, fabAirballoon));
+        }
+
+        private void ShowFabMenu()
+        {
+            isFabOpen = true;
+            fabAirballoon.Visibility = ViewStates.Visible;
+            fabCake.Visibility = ViewStates.Visible;
+            bgFabMenu.Visibility = ViewStates.Visible;
+
+            fabMain.Animate().Rotation(135f);
+            bgFabMenu.Animate().Alpha(1f);
+            fabAirballoon.Animate()
+                .TranslationY(-Resources.GetDimension(Resource.Dimension.standard_100))
+                .Rotation(0f);
+            fabCake.Animate()
+                .TranslationY(-Resources.GetDimension(Resource.Dimension.standard_55))
+                .Rotation(0f);
+        }
+
+        private class FabAnimatorListener : Java.Lang.Object, Animator.IAnimatorListener
+        {
+            View[] viewsToHide;
+
+            public FabAnimatorListener(params View[] viewsToHide)
+            {
+                this.viewsToHide = viewsToHide;   
+            }
+
+            public void OnAnimationCancel(Animator animation)
+            {
+
+            }
+
+            public void OnAnimationEnd(Animator animation)
+            {
+                if (!isFabOpen)
+                    foreach (var view in viewsToHide)
+                        view.Visibility = ViewStates.Gone;
+            }
+
+            public void OnAnimationRepeat(Animator animation)
+            {
+
+            }
+
+            public void OnAnimationStart(Animator animation)
+            {
+
+            }
         }
     }
 }
